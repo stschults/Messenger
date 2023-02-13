bc79efa3db0762b291815642ed6d3c22cb0b6437
Stanislav Schults <stschults@yandex.ru>
2023-02-13 18:45:31 +0300

    Initial commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Станислав Шульц on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
